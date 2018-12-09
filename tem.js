334df7cd02329ee55fbf818ee4fc9b7dfe2a6e60
Zophyr <zhangjliang@mail2.sysu.edu.cn>
2018-12-09 16:10:07 +0800

    Initial commit

---
+++ tem.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+    var elem = document.querySelector('input[type="range"]');
+
+    var rangeValue = function () {
+        var newValue = elem.value;
+        var target = document.querySelector('.value');
+        target.innerHTML = newValue;
+    }
+
+    elem.addEventListener("input", rangeValue);
+
+    var wrapper = $("#button-wrapper");
+
+    $(".submit").click(function () {
+        if (wrapper.not(".checked")) {
+            wrapper.addClass("checked");
+            setTimeout(function () {
+                wrapper.removeClass("checked");
+            }, 8000);
+        }
+    });
+}
