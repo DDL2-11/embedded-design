b2823e0b3b6d9723f37e8367900884dd4354a563
Zophyr <zhangjliang@mail2.sysu.edu.cn>
2018-12-09 18:47:05 +0800

    Update tem.js

--- tem.js
+++ tem.js
@@ -1,22 +1,29 @@
 window.onload = function () {
     var elem = document.querySelector('input[type="range"]');
+    var theTemp;
 
     var rangeValue = function () {
         var newValue = elem.value;
         var target = document.querySelector('.value');
+        theTemp = newValue;
         target.innerHTML = newValue;
     }
 
     elem.addEventListener("input", rangeValue);
 
-    // var wrapper = $("#button-wrapper");
+    var ws = new WebSocket("192.168.4.1:8080");
 
-    // $(".submit").click(function () {
-    //     if (wrapper.not(".checked")) {
-    //         wrapper.addClass("checked");
-    //         setTimeout(function () {
-    //             wrapper.removeClass("checked");
-    //         }, 8000);
-    //     }
-    // });
+    ws.onopen = function (evt) {
+        console.log("Connection open ...");
+        ws.send(theTemp);
+    };
+
+    ws.onmessage = function (evt) {
+        console.log("Received Message: " + evt.data);
+        ws.close();
+    };
+
+    ws.onclose = function (evt) {
+        console.log("Connection closed.");
+    };
 }
