b85eb4eace9893b8c0730caae1afdf977aacb781
Zophyr <zhangjliang@mail2.sysu.edu.cn>
2019-02-09 13:58:33 +0800

    Create choose.js

---
+++ choose.js
@@ -0,0 +1,18 @@
+$('document').ready(function () {
+    var loading = function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        e.target.classList.add('loading');
+        e.target.setAttribute('disabled', 'disabled');
+        setTimeout(function () {
+            e.target.classList.remove('loading');
+            e.target.removeAttribute('disabled');
+            window.location.href = './status.html'
+        }, 1500);
+    };
+
+    var btns = document.querySelectorAll('button');
+    for (var i = btns.length - 1; i >= 0; i--) {
+        btns[i].addEventListener('click', loading);
+    }
+});
